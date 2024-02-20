58cd1dc6ac83d2fff070ce0c16169278b47f1a52
o <o@LENOVO-pc-20>
2024-02-20 15:12:52 +0530

    SNEHA

---
+++ IntroductionToJava/src/com/edubridge/datatypes/DataTypesDemo.java
@@ -0,0 +1,17 @@
+package com.edubridge.datatypes;
+
+public class DataTypesDemo 
+{
+
+	public static void main(String[] args)
+    {
+		String Name = "Sneha Dhulap";
+		int Age = 20;
+		String Address = "Mankhurd Mumbai";
+		
+		
+		System.out.println("Name : "+Name+"\n Age : "+Age+"\n Address : "+Address);
+
+	}
+
+}
