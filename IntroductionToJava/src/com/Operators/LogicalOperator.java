9e4d753dc205d4e82bc1748853171fbfc8c3635e
o <o@LENOVO-pc-20>
2024-02-20 18:16:57 +0530

    sneha3

---
+++ IntroductionToJava/src/com/Operators/LogicalOperator.java
@@ -0,0 +1,16 @@
+package com.Operators;
+
+public class LogicalOperator
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(4>=5 && 4==4);
+		System.out.println(4>5 || 4==4);
+
+		System.out.println(!(7==5));
+
+
+	}
+
+}
---
+++ IntroductionToJava/src/com/Operators/RelationalOperator.java
@@ -0,0 +1,16 @@
+package com.Operators;
+
+import java.util.Scanner;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(1>=4);
+		System.out.println(8<7);
+		System.out.println(4==7);
+		System.out.println(0!=4);
+		System.out.println(26<9);
+	}
+
+}
